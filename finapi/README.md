12cc2d9611f19608bcd815fee57aab325afd46fd
Marcelllombm <melombm@hotmail.com>
2023-03-01 13:43:01 -0300

    criar um conta e não deve ser possivel cadastrar um conta com CPF ja existente

--- finapi/README.md
+++ finapi/README.md
@@ -2,7 +2,7 @@
 
 ### Requisitos
 
-- [] Deve ser possível criar um conta
+- [x] Deve ser possível criar um conta
 - [] Deve ser possível buscar o extrato  bancário  do cliente
 - [] Deve ser possível possível realizar  um depósito
 - [] Deve ser possível possível  um saque
@@ -13,7 +13,7 @@
 
 ## Regras de negócio
 
-- [] Não deve ser possível cadastrar  um conta  com CPF já existente
+- [x] Não deve ser possível cadastrar  um conta  com CPF já existente
 - [] Não deve ser possível fazer depósito em uma conta não existente
 - [] Não deve ser possível buscar extrato em um conta não existente
 - [] Não deve ser possível fazer saque em uma conta não existente
--- finapi/src/index.js
+++ finapi/src/index.js
@@ -4,5 +4,35 @@ const {v4: uuidv4} = require('uuid');
 const app = express();
 app.use(express.json());
 
+/*
+cpf = string
+name = string
+id - uuid
+statement []
+*/
+
+//guardar dados
+const customers = [];
+
+
+
+app.post ('/account',(request, response) =>{
+    const {cpf , name} = request.body;
+    
+    const customersAlreadyExist = customers.some((customer) => customer.cpf === cpf);
+    
+    if(customersAlreadyExist) {
+        return response.status(400).json({error: "Customer already exists!"})
+        } 
+    
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(), 
+        statement: []
+    });
+   
+    return response.status(201).send();
+})
 
 app.listen(3333);
