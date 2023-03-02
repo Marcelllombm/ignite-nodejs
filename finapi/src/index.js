12694cbc6985f3a6a6668a26bc57e5b387a07840
Marcelllombm <melombm@hotmail.com>
2023-03-02 10:09:39 -0300

    deve ser possivel realizar um saque em conta existente

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -24,6 +24,19 @@ function verifyInfExistsAccountCPF (request, response, next) {
     
 }
 
+function getBalance(statement){
+    console.log(statement.amount);
+    
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount ;
+        }
+    }, 0 );
+   
+    return balance;
+}
 
 app.post ('/account',(request, response) =>{
     const {cpf , name} = request.body;
@@ -49,9 +62,7 @@ app.post ('/account',(request, response) =>{
 app.get('/statement', verifyInfExistsAccountCPF, (request, response) => {
     
     const customer = request.customer;    
-    return response.status(200).json(customer.statement);
-    
-    
+    return response.status(200).json(customer.statement); 
 })
 
 app.post('/deposit',verifyInfExistsAccountCPF, (request, response) => {
@@ -69,4 +80,27 @@ app.post('/deposit',verifyInfExistsAccountCPF, (request, response) => {
     
     return response.status(201).send();
 })
+
+app.post('/withdraw',verifyInfExistsAccountCPF, (request, response)=>{
+const {amount} = request.body;
+const {customer} = request;
+const balance = getBalance(customer.statement);
+
+
+if(balance < amount) {
+return response.status(400).json({error: "Insufficient funds!"});
+}
+
+const statementOperations = {
+    amount,
+    create_at: new Date(),
+    type:"debit"
+}
+
+customer.statement.push(statementOperations);
+
+return response.status(201).send();
+})
+
+
 app.listen(3333);
